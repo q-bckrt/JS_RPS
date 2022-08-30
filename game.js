c7118a52ae24e7be1453b390acc9c7ef0ef556d1
q-bckrt <pom.schc@gmail.com>
2022-08-30 17:47:23 +0200

    advance on the gui game logic

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
+function toggleGameInterface () {
+    display_result.classList.toggle('hideSection');
+    display_score.classList.toggle('hideSection');
+    buttons_box.classList.toggle('hideSection');
+
+    playBtn.classList.toggle('hideSection');
+}
+
 // Get a random integer between 1 and 3, 
 // then converts it to 'rock', 'paper' or 'scissors' and returns it.
 function getComputerChoice(){
@@ -38,25 +46,28 @@ function playRoundGui (e) {
     }
     display_score.textContent = `computer: ${cmpScore} - user: ${usrScore}`;
     if (usrScore === 5 || cmpScore === 5) {
-        gameEndReset();
+        gameReset(true);
     }
 }
 
-function gameEndReset () {
-    if (usrScore > cmpScore) {
-        display_result.textContent = 'You win!';
-    } else {
-        display_result.textContent = 'You lose!';
-    }
+function gameReset (again) {
     usrScore = 0;
     cmpScore = 0;
+    toggleGameInterface();
+    playBtn.textContent = 'Play Again?';
 }
 
+
+// MAIN
 let usrScore = 0;
 let cmpScore = 0;
-    
+
 const display_result = document.querySelector('.dialog_box');
 const display_score = document.querySelector('.score_box');
+const buttons_box = document.querySelector('.buttons_box');
 
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('div.buttons_box > button');
 btns.forEach(e => e.addEventListener("click", playRoundGui));
+
+const playBtn = document.querySelector(".play_btn");
+playBtn.addEventListener("click", () => gameReset(true));
